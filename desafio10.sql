6887f11205886cf418315f88b339c1eb2447f0a3
Icaro Joel <icaromoura.programador@gmail.com>
2022-09-27 14:17:54 -0300

    10 and 11

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT 
+  m.nome AS 'nome',
+  COUNT(h.musica_id) AS 'reproducoes'
+FROM SpotifyClone.musica AS m
+INNER JOIN SpotifyClone.historico AS h
+  ON h.musica_id = m.id
+INNER JOIN SpotifyClone.usuario AS u
+  ON h.usuario_id = u.id
+INNER JOIN SpotifyClone.plano AS p
+  ON u.plano_id = p.id
+WHERE u.plano_id IN (1, 2)
+GROUP BY m.nome 
+ORDER BY m.nome ASC;
